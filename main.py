4563d9be056f253f7167422e2e622789ee13e9d5
proku <andrejames1021@gmail.com>
2022-09-12 21:24:24 +0800

    feature\search-function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from importlib.machinery import FileFinder
 from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip 
+import json 
 
 #---------------------------- PASSWORD GENERATOR ---------------------------#
  
@@ -31,22 +33,61 @@ def generate_password():
     
 
 #---------------------------- SAVE PASSWORD ---------------------------#
+
 def save():
 
     website = website_entry.get()
-    email = email_entry.get()
+    email = email_entry.get()              
     password = password_entry.get() 
-
+    new_data = { 
+        website: {
+            "email": email,
+            "password": password,
+            } 
+        }
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
             messagebox.askretrycancel(title="Oops", message="Please make sure you haven't left any fields empty.")
-    else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details you entered: \nEmail: {email} \nPassword: {password} \nIs it okay to save?:")
-            
-        with open(r"C:\Users\Hizon\Desktop\Coding\python\day-29-password-manager-gui\data.txt", "a") as data_file:
-            data_file.write(f"{website} | {email} | {password}")
-  
+    else:  
+        try:
+            with open(r"C:\Users\Hizon\Desktop\Coding\python\day-29-password-manager-gui\data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
 
-#---------------------------- UI SETUP ---------------------------#
+        except FileNotFoundError:
+            with open(r"C:\Users\Hizon\Desktop\Coding\python\day-29-password-manager-gui\data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+
+        else:
+            #Updating old data with new data
+            data.update(new_data)
+            with open(r"C:\Users\Hizon\Desktop\Coding\python\day-29-password-manager-gui\data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+        
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+            email_entry.delete(0, END)
+#---------------------------- FIND PASSWORD ---------------------------#
+
+def find_password():  
+    website = website_entry.get()
+    try:
+        with open(r"C:\Users\Hizon\Desktop\Coding\python\day-29-password-manager-gui\data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+
+    else:
+        if website in data:
+                email = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")      
+        else: 
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
+
+#----------------------------- UI SETUP -------------------------------#
 
 window = Tk()
 window.title("Password Manager")
@@ -67,19 +108,21 @@ password_label = Label(text="Password:", bg="#FFFFFF")
 password_label.grid(row=3, column=0)
 
 #Entries
-website_entry = Entry(width=40)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=30)
+website_entry.grid(row=1, column=1, columnspan=1)
 website_entry.focus()
-email_entry = Entry(width=40)
+email_entry = Entry(width=48)
 email_entry.grid(row=2, column=1, columnspan=2)
-password_entry = Entry(width=40)
-password_entry.grid(row=3, column=1, columnspan=2)
+email_entry.insert(0, "2022308474@dhvsu.edu.ph")
+password_entry = Entry(width=30)
+password_entry.grid(row=3, column=1, columnspan=1)
 
 #Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password, width=14)
-generate_password_button.grid(row=4, column=1, columnspan=1)
-add_button = Button(text="Add", width=36, command=save)
+generate_password_button.grid(row=3, column=2, columnspan=1)
+add_button = Button(text="Add", width=40, command=save)
 add_button.grid(row=5, column=1, columnspan=2)
-
+search_button = Button(text="Search", width=14, command=find_password)
+search_button.grid(row=1, column=2, columnspan=1)
 
 window.mainloop() 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,5 @@
-   # for n in password_list:
-    #     password += n
+# Reading old data
+            # data = json.load(data_file)
+        
+            # #Updating old data with new data
+            # data.update(new_data)
